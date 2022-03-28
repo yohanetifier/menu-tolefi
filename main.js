c6bfb47a89926ab1fa73cf020fc3c678724bccc3
Yohan Etifier <yohan@adelios.fr>
2022-03-28 22:10:46 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 const realEstate = document.querySelector(".left-section__text--real-estate")
-const middleSection = document.querySelector('.middle-section')
 const contentRealEstate= document.querySelector(".middle-section__content")
-const listContent= document.querySelectorAll(".middle-section__content > p")
 const retailDistribution= document.querySelector(".left-section__text--retail-distribution")
 const contentRetailDistribution= document.querySelector(".middle-section__contentone")
-//  const deplierSection = document.querySelector(".deplier")
-const plus = document.querySelector('.middle-section__plus > p')
-const inputChecked = document.querySelector('input')
-const subMiddleSection = document.querySelector('.middle-section__plus')
-// const testSvg = document.querySelector('.testsvg1')
-
+const financeInvestment = document.querySelector(".left-section__text--finance")
+const contentFinanceInvestment = document.querySelector('.middle-section__contenttwo')
 
 class Menu{
     
@@ -17,46 +11,43 @@ class Menu{
         this.element = element
         this.toggle = toggle
         this.parent = this.toggle.parentNode
+        this.childDivParent = this.parent.querySelectorAll('div')
+        this.allSvg = this.parent.querySelectorAll('svg')
         this.subdivision = this.element.textContent.split(' ').join('-').toLowerCase()
-        this.test = this.parent.querySelectorAll('.toggle')
         this.subElement = document.querySelector(`.middle-section__plus--${this.subdivision}`)
         this.svg = document.querySelector(`.middle-section__plus--${this.subdivision} > svg:nth-child(2)`)
-
-        // this.svg = document.querySelector(`${this.subElement} > svg:nth-child(2)`)
     }
     mouseOver(){
-        this.element.addEventListener("mouseover", () => {
-            this.toggle.style.opacity = 1;
-            this.toggle.style.transition = `opacity 300ms`
-            this.svg.style.transform = `rotate(180deg)`
-            console.log(this.test)
-         })
-        //  this.subElement.addEventListener('mouseover', () => {
-        //             this.toggle.style.opacity = 1
-        //         })
-    }
-    mouseLeave(){
-        this.element.addEventListener('mouseleave', () => {
-            this.toggle.style.opacity = 0;
-            this.toggle.style.transition = `opacity 300ms`
-            this.svg.style.transform = `rotate(90deg)`
-
+        this.subElement.addEventListener('click', () => {
+            // const test = this.subElement.querySelectorAll('svg')
+            
+            //Check if class active already on
+           
+            for (let i = 0; i < this.childDivParent.length; i++){
+                for(let j = 0; j < this.allSvg.length; j++){
+                    if(this.childDivParent[i].classList.contains('active') && this.allSvg[j].classList.contains('activeSvg')){
+                        this.childDivParent[i].classList.remove('active')
+                         this.allSvg[j].classList.remove('activeSvg')
+                        this.svg.classList.add('activeSvg')
+                        this.toggle.classList.add('active')
+                    }else {
+                    this.toggle.classList.add('active')
+                    this.svg.classList.add('activeSvg')
+                    }
+                }
+            }
         })
-    }
+    }    
 }
 
 
 const blockOne = new Menu(realEstate, contentRealEstate)
 const blockTwo = new Menu(retailDistribution, contentRetailDistribution)
+const blockThree = new Menu(financeInvestment,contentFinanceInvestment)
 
 blockOne.mouseOver()
-blockOne.mouseLeave()
-// blockOne.mouseLeave()
-// blockOne.keepSectionActive()
-// blockOne.closeSectionActive()
-// blockOne.verifyVisible()
-// blockTwo.mouseOver()
-// blockTwo.mouseLeave()
+blockTwo.mouseOver()
+blockThree.mouseOver()
 
 
 
